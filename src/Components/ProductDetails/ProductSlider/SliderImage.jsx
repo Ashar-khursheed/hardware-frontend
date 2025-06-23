7c40e7d5dc1850ce60e8c91eb930db52f5096c9f
AsharKhursheed <asharkhursheed26@gmail.com>
2025-06-24 02:28:33 +0500

    dsd

--- src/Components/ProductDetails/ProductSlider/SliderImage.jsx
+++ src/Components/ProductDetails/ProductSlider/SliderImage.jsx
@@ -1,3 +1,6 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import { ProductDetailTopSlider } from "@/Data/SliderSetting";
 import Image from "next/image";
 import { useState } from "react";
