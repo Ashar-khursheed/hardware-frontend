0a867940b18ea465b0e3faeb48e98ac9cc5b488e
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-04-23 20:31:57 +0500

    sd

--- src/Components/Themes/Electronics/ElectronicsThree/index.jsx
+++ src/Components/Themes/Electronics/ElectronicsThree/index.jsx
@@ -868,7 +868,7 @@ const ElectronicsThree = () => {
                   <div>
                     <h4 className="fw-bold text-black mb-1">Email Address</h4>
                     <p className="text-muted mb-0">
-                      <a style={{ color: "#212529bf" }} href="mailto:hello@thehardwarebox.com">hello@thehardwarebox.com</a>
+                      <a style={{ color: "#212529bf" }} href="mailto:support@thehardwarebox.com">support@thehardwarebox.com</a>
                     </p>
 
                   </div>
