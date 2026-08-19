845048ef257be39b6b723c2abe21c9ccd4d1572c
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-08-19 12:44:49 +0500

    asd

--- src/app/layout.js
+++ src/app/layout.js
@@ -107,13 +107,7 @@ export default async function RootLayout({ children }) {
             gtag('config', 'G-C52RSE232C');
           `}
         </Script>
-        <Script
-          src="https://api.luziostore.com/widget.js"
-          data-agent-id="019f6660-eb74-7271-94b1-6e0ecc293b29"
-          data-api-base="https://api.luziostore.com/api/v1"
-          async
-          strategy="afterInteractive"
-        />
+        <script src="https://www.convobix.com/widget.js" data-agent-id="019f6660-eb74-7271-94b1-6e0ecc293b29" data-api-base="https://www.convobix.com/api/v1" async></script>
 
         <meta name="google-site-verification" content="epeq1bBiL7iMs09lWmDHbnljSVjNdJLpxky4us0lvko" />
 
